a7676712bb999c94cda6642d2eb8d569969f9b22
thieudxqph21179 <115914547+thieudxqph21179@users.noreply.github.com>
2023-06-03 10:11:32 +0700

    test-commit

--- BackEnd/src/controllers/auth.js
+++ BackEnd/src/controllers/auth.js
@@ -4,43 +4,44 @@ import { SigninSchema, SignupSchema } from '../schamas/auth';
 import jwt  from 'jsonwebtoken';
 
 export const signup = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        const { error } = SignupSchema.validate(req.body, { abortEarly: false });
-        // if (error) {
-        //     const errors = error.details.map((err) => err.message);
-        //     return res.status(400).json({
-        //         message: errors,
-        //     });
-        // }
+    // try {
+    //     const { name, email, password } = req.body;
+    //     const { error } = SignupSchema.validate(req.body, { abortEarly: false });
+    //     if (error) {
+    //         const errors = error.details.map((err) => err.message);
+    //         return res.status(400).json({
+    //             message: errors,
+    //         });
+    //     }
 
-        const userExist = await User.findOne({ email });
-        if (userExist) {
-            return res.status(400).json({
-                message: 'Email đã tồn tại',
-            });
-        }
+    //     const userExist = await User.findOne({ email });
+    //     if (userExist) {
+    //         return res.status(400).json({
+    //             message: 'Email đã tồn tại',
+    //         });
+    //     }
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+    //     const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = await User.create({
-            name,
-            email,
-            password: hashedPassword,
-        });
+    //     const user = await User.create({
+    //         name,
+    //         email,
+    //         password: hashedPassword,
+    //     });
 
-        //Không trả password
-        user.password = undefined;
+    //     //Không trả password
+    //     user.password = undefined;
 
-        return res.status(201).json({
-            message: 'Đăng kí thành công',
-            user,
-        });
-    } catch (error) {
-        return res.status(400).json({
-            message: error,
-        });
-    }
+    //     return res.status(201).json({
+    //         message: 'Đăng kí thành công',
+    //         user,
+    //     });
+    // } catch (error) {
+    //     return res.status(400).json({
+    //         message: error,
+    //     });
+    // }
+    //ahhaa
 };
 
 export const signin = async (req, res) => {

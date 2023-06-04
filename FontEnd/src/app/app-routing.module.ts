f03ef0b49b367e22f0cac173c36c4eb9b4471c7c
Anhquy23 <Ttm1022013@gmail.com>
2023-06-04 19:09:36 +0700

    feat: create-UI-Layout-Admin refs: FPT-15

--- FontEnd/src/app/app-routing.module.ts
+++ FontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: AdminLayoutComponent },
+      
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

8c505a2e3c6e2e78687da04ceecdd1033d5dd7d1
thieudxqph21179 <115914547+thieudxqph21179@users.noreply.github.com>
2023-06-05 21:03:22 +0700

    feat: website-layout refs: FPT-27

--- FontEnd/src/app/app-routing.module.ts
+++ FontEnd/src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { WebsiteLayoutComponent } from './layout/website-layout/website-layout.component';
+import { HomePageComponent } from './pages/client/home-page/home-page.component';
+import { ProductsPageComponent } from './pages/client/products-page/products-page.component';
+import { ProductComponent } from './pages/client/product/product.component';
 
 const routes: Routes = [
   {
@@ -9,8 +13,14 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: AdminLayoutComponent },
-      
     ],
+  },
+  {
+    path: "", component: WebsiteLayoutComponent, children: [
+      { path: "", component: HomePageComponent },
+      { path: "products", component: ProductsPageComponent },
+      { path: "product/:id", component: ProductComponent }
+    ]
   },
 ];
 
--- FontEnd/src/app/app.module.ts
+++ FontEnd/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { WebsiteLayoutComponent } from './layout/website-layout/website-layout.component';
+import { HomePageComponent } from './pages/client/home-page/home-page.component';
+import { ProductsPageComponent } from './pages/client/products-page/products-page.component';
+import { ProductComponent } from './pages/client/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    WebsiteLayoutComponent,
+    HomePageComponent,
+    ProductsPageComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,

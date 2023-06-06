4525003975791c02efff13729e8d77ccc317e2c5
thieudxqph21179 <115914547+thieudxqph21179@users.noreply.github.com>
2023-06-06 17:47:10 +0700

    feat: listproducts-and-productdetail refs: FPT-34

--- FontEnd/src/app/interface/Product.ts
+++ FontEnd/src/app/interface/Product.ts
@@ -1,5 +1,5 @@
 export interface IProduct {
-    _id?: number;
+    _id?: string;
     name: string;
     author: string;
     price: number;
--- FontEnd/src/app/pages/client/product/product.component.ts
+++ FontEnd/src/app/pages/client/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/interface/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
+  product!: IProduct;
+  
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {
+    this.route.paramMap.subscribe(param => {
+      const id = param.get('id') ;
+      this.productService.getProductById(id).subscribe(product => {
+        this.product = product;
+      }, error => console.log(error.message))
+    })
+  }
+  }
 
-}
